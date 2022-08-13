a14816c5a59c24e369a9101c3553ceddcb527aa7
ivankrister <ivankrister.garica@gmail.com>
2022-08-13 11:55:41 +0800

    resource

--- app/Http/Controllers/Managements/CategoriesController.php
+++ app/Http/Controllers/Managements/CategoriesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Managements;
 
 use App\Http\Requests\CategoryRequest;
+use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 
 class CategoriesController extends \App\Http\Controllers\Controller
@@ -10,7 +11,7 @@ class CategoriesController extends \App\Http\Controllers\Controller
 
     public function index()
     {
-        return Category::query()->orderBy('name','asc')->get();
+        return CategoryResource::collection(Category::query()->orderBy('name','asc')->get());
     }
 
     public function store(CategoryRequest $request)
@@ -18,7 +19,7 @@ class CategoriesController extends \App\Http\Controllers\Controller
         $category = new Category;
         $category->name = $request->name;
         $category->save();
-        return $category;
+        return CategoryResource::make($category);
     }
 
     public function update(CategoryRequest $request, int $id)
@@ -26,13 +27,13 @@ class CategoriesController extends \App\Http\Controllers\Controller
         $category = Category::query()->findOrFail($id);
         $category->name = $request->name;
         $category->save();
-        return $category;
+        return CategoryResource::make($category);
     }
 
     public function destroy(int $id)
     {
         $category = Category::query()->findOrFail($id);
         $category->delete();
-        return $category;
+        return CategoryResource::make($category);
     }
 }
