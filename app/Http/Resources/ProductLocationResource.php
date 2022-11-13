917d6c61d1113fde990b6c6674259247795a54d4
ivankrister <ivankrister.garica@gmail.com>
2022-11-13 11:20:30 +0800

    psr-4

--- app/Http/Resources/ProductLocationResource.php
+++ app/Http/Resources/ProductLocationResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class InventoryLocationResource extends JsonResource
+class ProductLocationResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
